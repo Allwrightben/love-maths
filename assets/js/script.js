bbc3238103e252f2f498d03c79e41c4d10d0cdd1
Allwrightben <ben.allwright@learningpeople.co.uk>
2023-12-09 12:49:08 -0600

    Add DOMContentLoaded listener to trigger the first function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame() {
 
 }
@@ -17,6 +32,7 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
+
 function displayAditionQuestion() {
 
 }
