c7bdf2f677a3bf8d791658f0d44aa74ef86abad4
Allwrightben <ben.allwright@learningpeople.co.uk>
2023-12-09 13:01:06 -0600

    Add num1 and num2 random number code

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+/**
+ * The main game "loop", called when script is first loaded
+ * and after the userts answer has been processed
+ */
 function runGame() {
-
+    let num1 = Math.floor(Math.random() * 25);
+    let num2 = Math.floor(Math.random() * 25);
 }
 
 function checkAnswer() {
