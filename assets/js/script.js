04410dc88fe2d2403e3fd2cc49ab79e8039d77f9
Allwrightben <ben.allwright@learningpeople.co.uk>
2023-12-11 14:47:25 -0600

    Add increment score and incorrect answer code

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,9 +44,11 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! you got it right! :)")
+        incrementScore();
     } else {
         alert(`Awww you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
-    }
+        incrementWrongAnswer();
+        }
 
     runGame(calculatedAnswer[1]);
 
@@ -70,11 +72,13 @@ function calculateCorrectAnswer() {
 }
 
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
 
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
